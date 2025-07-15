0f5c65833bee220eb07793195eb9a2ee7e547438
unknown <david.barbosa-g@labinfo.is.escuelaing.edu.co>
2025-07-15 10:03:17 -0500

    first commit

---
+++ src/main/java/com/eci/ARSW/redisPublishSubscribe/RedisPublishSubscribeApplication.java
@@ -0,0 +1,13 @@
+package com.eci.ARSW.redisPublishSubscribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisPublishSubscribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisPublishSubscribeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eci/ARSW/redisPublishSubscribe/RedisPublishSubscribeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eci.ARSW.redisPublishSubscribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisPublishSubscribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
